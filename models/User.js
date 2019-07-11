06da9ae2c358b78ffbdf0ada3c0022b05c8b58aa
Ravi Kallepalli <ravi@austinwebdeveloper.co>
2019-07-11 12:18:11 +0530

    backend

--- models/User.js
+++ models/User.js
@@ -1,20 +1,22 @@
-let mongoose = require('mongoose');
+let mongoose = require("mongoose");
 let SChema = mongoose.Schema;
 const User = new SChema({
-  fullName: {type: String, required: true},
-  email: {type: String, unique: true, required: true},
-  password: {type: String, required: true},
-  category: {type: String, required: true},
-  phone: {type: String, unique: true, required: true},
-  address: {type: String},
-  twilioStatus: {type: Boolean, default: false},
-  latitude: {type: Number},
-  longitude: {type: Number},
-  date_of_birth: {type: String},
-  blood_Group: {type: String},
-  language: {type: String},
-  company_name: {type: String},
-  experience: {type: Number}
+  fullName: { type: String, required: true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
+  category: { type: String, required: true },
+  phone: { type: String, unique: true, required: true },
+  city: { type: String },
+  state: { type: String },
+  zip: { type: Number },
+  twilioStatus: { type: Boolean, default: false },
+  latitude: { type: Number },
+  longitude: { type: Number },
+  date_of_birth: { type: String },
+  blood_Group: { type: String },
+  language: { type: String },
+  company_name: { type: String },
+  experience: { type: Number }
 });
-const Users = mongoose.model('user', User);
+const Users = mongoose.model("user", User);
 module.exports = Users;
